f727bf4e343d0272ab19c7b5706c43dcfea9a638
dips <dipeshshrestha910@gmail.com>
2025-01-16 23:09:45 +0545

    added image support

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -4,6 +4,8 @@ import TaskList from "@tiptap/extension-task-list";
 import TaskItem from "@tiptap/extension-task-item";
 import StarterKit from "@tiptap/starter-kit";
 import Table from '@tiptap/extension-table'
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image'
 import TableCell from '@tiptap/extension-table-cell'
 import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
@@ -16,7 +18,7 @@ export const Editor = () => {
           "focus:outline-none print:border-0 bg-white border border-[#c7c7c7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text ",
       },
     },
-    extensions: [StarterKit, TaskItem.configure({ nested: true }), TaskList, Table, TableCell, TableHeader, TableRow],
+    extensions: [StarterKit,Image,ImageResize, TaskItem.configure({ nested: true }), TaskList, Table, TableCell, TableHeader, TableRow],
     content: ` <table>
           <tbody>
             <tr>
