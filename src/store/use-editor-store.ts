6bde1e0a64d02c957978c2c4cf507ed006772b67
dips <dipeshshrestha910@gmail.com>
2025-01-16 23:51:21 +0545

    added zustland and undo button

---
+++ src/store/use-editor-store.ts
@@ -0,0 +1,14 @@
+import {create} from 'zustand';
+import {type Editor}from "@tiptap/react";
+
+
+interface EditorState{
+  editor: Editor | null;
+  setEditor: (editor: Editor) => void;
+};
+export const useEditorStore = create<EditorState>((set) => ({
+editor: null,
+setEditor: (editor: Editor) => set({editor}),
+
+}));
+
